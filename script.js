d0cd72794bc9f9321f8e609128dd207f617c16c9
Sahil Agrawal <technicalgamer445@gmail.com>
2026-08-17 23:13:09 +0545

    refactor: reformat navigation animation code and encapsulate page 2 animation into a reusable function

--- script.js
+++ script.js
@@ -1,66 +1,72 @@
-function navAnimation(){
-    let Nav = document.querySelector(".nav");
+function navAnimation() {
+  let Nav = document.querySelector(".nav");
 
-Nav.addEventListener("mouseenter", function(){
-const tl = gsap.timeline();
+  Nav.addEventListener("mouseenter", function () {
+    const tl = gsap.timeline();
 
-    tl.to(".nav-bottom",{
-        height: "26vh",
-    }).to(".nav-menu h5", {
+    tl.to(".nav-bottom", {
+      height: "26vh",
+    })
+      .to(".nav-menu h5", {
         display: "block",
-    }).to(".nav-menu h5 span", {
+      })
+      .to(".nav-menu h5 span", {
         y: 0,
         // duration: 0.2,
         stagger: {
-            amount: 0.3
-        }
-    })
-})
+          amount: 0.3,
+        },
+      });
+  });
 
-Nav.addEventListener("mouseleave", function(){
+  Nav.addEventListener("mouseleave", function () {
     const tl = gsap.timeline();
 
     tl.to(".nav-menu h5 span", {
-        y: 20,
-        stagger: {
-            amount: 0.2
-        }
-    })
+      y: 20,
+      stagger: {
+        amount: 0.2,
+      },
+    });
     tl.to(".nav-menu h5", {
-        display: "none",
-        duration: 0.1,
-    })
-    tl.to(".nav-bottom",{
-        height: "0vh",
-        duration: 0.2,
-    })
-})
+      display: "none",
+      duration: 0.1,
+    });
+    tl.to(".nav-bottom", {
+      height: "0vh",
+      duration: 0.2,
+    });
+  });
 }
 
-// navAnimation();
+navAnimation();
 
-let relem = document.querySelectorAll(".right-elem")
-let relemImg = document.querySelectorAll(".right-elem img")
+function page2Animation() {
+  let relem = document.querySelectorAll(".right-elem");
+  let relemImg = document.querySelectorAll(".right-elem img");
 
-relem.forEach(function (elems){
-    elems.addEventListener("mouseenter", function(newelems){
-       gsap.to(elems.childNodes[3], {
-            opacity: 1,
-            scale: 1
-        });
-    })
+  relem.forEach(function (elems) {
+    elems.addEventListener("mouseenter", function (newelems) {
+      gsap.to(elems.childNodes[3], {
+        opacity: 1,
+        scale: 1,
+      });
+    });
 
-    elems.addEventListener("mouseleave", function(newelems){
-       gsap.to(elems.childNodes[3], {
-            opacity: 0,
-            scale: 0
-        });
-    })
+    elems.addEventListener("mouseleave", function (newelems) {
+      gsap.to(elems.childNodes[3], {
+        opacity: 0,
+        scale: 0,
+      });
+    });
 
-    elems.addEventListener("mousemove", function(dets){
-        gsap.to(elems.childNodes[3], {
-            x: dets.x - elems.getBoundingClientRect().x - 10,
-            y: dets.y - elems.getBoundingClientRect().y - 90
-        });
-    })
-})
+    elems.addEventListener("mousemove", function (dets) {
+      gsap.to(elems.childNodes[3], {
+        x: dets.x - elems.getBoundingClientRect().x - 10,
+        y: dets.y - elems.getBoundingClientRect().y - 90,
+      });
+    });
+  });
+}
+
+page2Animation();
